a96c696c39e1a2fb91b274591386018aea0a68cd
Brian Caterfino <bcaterfino@gmail.com>
2018-03-20 15:52:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+ function addToCart(item){
+  var price = Math.floor(Math.random()*99)
+  cartrt.push(new Object({[item]:price}))
+  console.log(`${item} has been added to your cart.`)
+  return cart
 }
-
 function viewCart() {
   // write your code here
 }
